7c895e6c2646d71aeabb388a7202eb6f99fc0394
dzhao1 <dzhao1@yinlu.com>
2024-09-11 08:35:11 +0000

    chore: Update logic deletion intercept module and table initialization

--- src/domain/intercept/logic_del_intercept.rs
+++ src/domain/intercept/logic_del_intercept.rs
@@ -6,7 +6,7 @@ use rbs::Value;
 #[derive(Debug)]
 pub struct LogicDelIntercept;
 #[async_trait]
-impl Intercept  for LogicDelIntercept {
+impl Intercept for LogicDelIntercept {
     async fn before(
         &self,
         task_id: i64,
@@ -15,17 +15,16 @@ impl Intercept  for LogicDelIntercept {
         args: &mut Vec<Value>,
         result: ResultType<&mut Result<ExecResult, Error>, &mut Result<Vec<Value>, Error>>,
     ) -> Result<Option<bool>, Error> {
-        println!("before task_id:{}",task_id);
-        println!("before sql:{}",sql);
-        println!("before args:{:?}",args);
-        println!("before result:{:?}",result);
+        println!("before task_id:{}", task_id);
+        println!("before sql:{}", sql);
+        println!("before args:{:?}", args);
+        println!("before result:{:?}", result);
         //当前sql为delete，则修改为update 语句 del_flag = 1
         if sql.contains("delete") {
             *sql = sql.replace("delete", "update");
             sql.push_str(" set del_flag = 1");
-        }
-        //当前sql为select，添加过滤条件 del_flag = 0
-        if sql.contains("select") {
+        } else if sql.contains("select") {
+            //当前sql为select，添加过滤条件 del_flag = 0
             if sql.contains("where") {
                 sql.push_str(" and del_flag = 0");
             } else {
@@ -34,11 +33,18 @@ impl Intercept  for LogicDelIntercept {
         }
         Ok(Some(true))
     }
-    async fn after(&self, task_id: i64, _rb: &dyn Executor, sql: &mut String, args: &mut Vec<Value>, result: ResultType<&mut Result<ExecResult, Error>, &mut Result<Vec<Value>, Error>>) -> Result<Option<bool>, Error> {
-        println!("after task_id:{}",task_id);
-        println!("after sql:{}",sql);
-        println!("after args:{:?}",args);
-        println!("after result:{:?}",result);
+    async fn after(
+        &self,
+        task_id: i64,
+        _rb: &dyn Executor,
+        sql: &mut String,
+        args: &mut Vec<Value>,
+        result: ResultType<&mut Result<ExecResult, Error>, &mut Result<Vec<Value>, Error>>,
+    ) -> Result<Option<bool>, Error> {
+        println!("after task_id:{}", task_id);
+        println!("after sql:{}", sql);
+        println!("after args:{:?}", args);
+        println!("after result:{:?}", result);
         Ok(Some(true))
     }
 }
